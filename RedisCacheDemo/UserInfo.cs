436eccb3873e04b3832423106f6ca6a3b3dc2493
zhaijingfu <zhaijingfu@outlook.com>
2018-11-19 14:37:35 +0800

    Add project files.

---
+++ RedisCacheDemo/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedisCacheDemo
+{
+    [Serializable]
+    public class UserInfo
+    {
+        public int Id;
+        public string UserName;
+        public int Age;
+    }
+}
